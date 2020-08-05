8f03e3e438520b48bb3c05cd5210441ed944f953
Tobi Weinstock <tobi.weinstock@PAR-MBA-TW-Loaner.home>
2020-08-05 20:39:31 +0200

    discovering tailwind styles

--- src/Session.js
+++ src/Session.js
@@ -4,10 +4,14 @@ import Timer from "./Timer";
 import "./App.css";
 
 const Session = () => (
-  <div>
-    <p>1/10 Question</p>
-    <Timer />
-    <Question />
+  <div className="m-8">
+    <div className="flex flex-row justify-around">
+      <p>1/10 Question</p>
+      <Timer />
+    </div>
+    <div>
+      <Question />
+    </div>
   </div>
 );
 
